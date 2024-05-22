e8eb470e30750311731dc111fc4edfcd1fc8dcaa
Extraskilled56 <zjones.092912@gmail.com>
2024-05-22 18:45:17 -0500

    update 0.5.5

--- runpkgs.py
+++
@@ -1,9 +0,0 @@
-import os
-def run_FusionGamesXCLI():
-    os.system("python3 Fusion-Games-X-CLI/FusionGamesXCLI.py")
-    
-def run_pyCalculate():
-    os.system("python3 pycalculate/main.py")
-    
-def run_gitpython():
-    os.system("python3 git-python/git-python.py")
