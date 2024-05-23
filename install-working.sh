6d4af82ede96310693ea1aec1dd2d2a57f3b8aee
Extraskilled56 <zjones.092912@gmail.com>
2024-05-23 09:33:01 -0500

    sdhjadk

--- install-working.sh
+++ install-working.sh
@@ -17,15 +17,13 @@ sudo mkdir poss
 cd poss
 sudo mkdir installed-pkgs
 cd ~
-cd /usr/bin/poss
-sudo mkdir package-index
 
 ### Cloning the repository
 
 
 cd /usr/bin/poss/
 git clone -b working https://git.bitsyncdev.com/bit-sync/Poss-v2
-cd /isr/bin/poss/package-index
+cd /isr/bin/poss/Poss-v2
 git clone https://pindex.bitsyncdev.com/poss-package-index/package-index
 
 ### Installing Poss-v2
