d9c13c1d18aaa753c2700b918f164f700ae50a0f
Extraskilled56 <zjones.092912@gmail.com>
2024-05-22 19:12:26 -0500

    idk

--- poss.py
+++ poss.py
@@ -9,14 +9,14 @@ def cli():
 @click.argument('package')
 def install(package):
     if package == 'pycalculate':
-        os.system("cd /usr/bin/poss/installed-pkgs/")
+        os.system("cd /usr/bin/poss/package-index/pycalculate")
         os.system("sudo bash install.sh")
 
 @cli.command()
 @click.argument('package')
 def run(package):
     if package == 'pycalculate':
-        os.system("cd /usr/bin/poss/package-index/pycalculate/")
+        os.system("cd /usr/bin/poss/package-index/pycalculate")
         os.system("sudo bash run.sh")
     else:
         print("Package not found")
@@ -25,8 +25,8 @@ def run(package):
 @click.argument('package')
 def uninstall(package):
     if package == 'pycalculate':
-        os.system("cd /usr/bin/poss/package-index/pycalculate/")
-        os.system("sudo bash uninstall.sh")
+        os.system("cd /usr/bin/poss/package-index/pycalculate")
+        os.system("sudo bash remove.sh")
     else:
         print("Package not found")
         
@@ -36,10 +36,11 @@ def version():
     
 @cli.command()
 def setup():
+    """
     os.system("mkdir poss")
     os.system("sudo mv poss /usr/bin/")
     os.system("mkdir installed-pkgs")
     os.system("sudo mv installed-pkgs /usr/bin/poss")
     print("Setup complete! (please do not run this again, might cause issues)")
-    
+    """
     
