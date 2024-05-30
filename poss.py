5fca46bb44aee0ebf5b1409db627e02e548da2ae
Extraskilled56 <zjones.092912@gmail.com>
2024-05-30 11:48:45 -0500

    another bug fix

--- poss.py
+++ poss.py
@@ -17,7 +17,7 @@ def install(package):
 @click.argument('package')
 def run(package):
     if package == 'pycalculate':
-        os.system("cd /usr/bin/poss/pindex/pycalculate")
+        os.system("cd /usr/bin/poss/Poss-v2/pindex/pycalculate")
         os.system("sudo bash run.sh")
     else:
         print("Package not found")
@@ -26,7 +26,7 @@ def run(package):
 @click.argument('package')
 def uninstall(package):
     if package == 'pycalculate':
-        os.system("cd /usr/bin/poss/package-index/pycalculate")
+        os.system("cd /usr/bin/poss/pindex/pycalculate")
         os.system("sudo bash remove.sh")
     else:
         print("Package not found")
